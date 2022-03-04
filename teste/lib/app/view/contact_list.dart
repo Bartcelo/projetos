aef7dcd8ddd7686a4c9eab54a92c88ed388e5e91
Bartcelo <44444081+Bartcelo@users.noreply.github.com>
2022-03-03 21:46:33 -0300

    Remoção loremy

--- teste/lib/app/view/contact_list.dart
+++ teste/lib/app/view/contact_list.dart
@@ -113,12 +113,10 @@ class _ContactListState extends State<ContactList> {
                             GestureDetector(
                               onTap: () {
                                 setState(() {
-                                  _height == 100 ?_height = 400 : _height = 100;
-                                 
-                                  }
-                                );
-
-                                
+                                  _height == 100
+                                      ? _height = 400
+                                      : _height = 100;
+                                });
                               },
                               child: AnimatedContainer(
                                   padding: const EdgeInsets.all(0),
@@ -139,7 +137,7 @@ class _ContactListState extends State<ContactList> {
                                     child: Column(
                                       children: [
                                         Text(
-                                          " Informações: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" +
+                                          " Informações:" +
                                               contato.informa.toString(),
                                           style: const TextStyle(
                                               color: Colors.white,
