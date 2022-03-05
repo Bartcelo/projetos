ecf11b3809ff1706747d398e141a4dfefae39922
Bartcelo <44444081+Bartcelo@users.noreply.github.com>
2022-03-05 10:40:16 -0300

    04/03/22 listas em dart

--- aprendaDart/index.dart
+++ aprendaDart/index.dart
@@ -2,19 +2,25 @@ void  main() {
   //var nomeCompleto = "Marcelo gonzaga";
   //Object nomeCompleto = "Raiane Gonzaga";
   //String nomeCompleto = "Marcelo e Raiane";
-  dynamic nomeCompleto = "Raiane e Marcelo";
-   print(nomeCompleto);
+  //dynamic nomeCompleto = "Raiane e Marcelo";
+  // print(nomeCompleto);
    
-   int idade = 12;
-   print(idade);
-   String vida = "minha vida";
-   print(vida);
+  // int idade = 12;
+  // print(idade);
+  // String vida = "minha vida";
+   //print(vida);
 
    // Numeros
    // inteiros  doble
 
-   double valor = 10.50;
-   print(valor.isNaN);
+  // double valor = 10.50;
+ //  print(valor);
+
+
+ bool valor = true;
+
+ print(valor.toString() == "true");
+
 
    
  
