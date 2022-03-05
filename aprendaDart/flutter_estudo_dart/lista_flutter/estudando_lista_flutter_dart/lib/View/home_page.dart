85c82af4d23ad63972b32431c62e451e1569016b
Bartcelo <44444081+Bartcelo@users.noreply.github.com>
2022-03-05 17:28:48 -0300

    05/03/22 Estudo de Lista ListTile Fundo vermelho

---
+++ aprendaDart/flutter_estudo_dart/lista_flutter/estudando_lista_flutter_dart/lib/View/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+List<String> listaNome = ["Raiane", "Marcelo", "Marli", "Boaz"];
+
+List<Widget> listaWidget = listaNome.map((nome) => ListTile(title: Text(nome),leading:Icon (Icons.person) ,tileColor: Colors.red[100],)).toList();
+
+
+
+// List<Widget> ListaWidget = [
+//   ListTile(title: Text(listaNome[0]),leading:Icon (Icons.person) ,tileColor: Colors.red[100],),
+//   ListTile(title: Text(listaNome[1]),leading:Icon (Icons.person) ,tileColor: Colors.red[100],),
+//   ListTile(title: Text(listaNome[2]),leading:Icon (Icons.person) ,tileColor: Colors.red[100],),
+//   ListTile(title: Text(listaNome[3]),leading:Icon (Icons.person) ,tileColor: Colors.red[100],),
+// ];
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista em Flutter'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [...listaWidget],
+        ),
+      ),
+    );
+  }
+}
