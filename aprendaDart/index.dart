794f2480e15d0e208334db0915675e9fd7935af7
Bartcelo <44444081+Bartcelo@users.noreply.github.com>
2022-03-05 11:44:12 -0300

    05/03/22 estudo de SETS

--- aprendaDart/index.dart
+++ aprendaDart/index.dart
@@ -1,33 +1,12 @@
 
 
 void  main() {
- // estudando listas
-List <int> lista = [1,2,3];
-List <int> lista2 = [7,8,9,10,11,12];
+ // estudo de sets com dart
 
-print(lista);
-print("ok1");
-print(lista.remove(2));
-print("ok2");
-lista.add(5);
-print(lista);
-print("ok3");
-lista.clear();
-print(lista);
-print("ok4");
-lista.addAll(lista2);
-print("ok5");
-var elemento = lista.elementAt(1);
-print(elemento);
+  Set <String> nomes = {};
 
-print("ok6");
-print(lista);
-print("ok7");
-var nova = null;
-var listatotal = ["Estidando lista",...lista.reversed, ...lista2, "com dart", ...?nova];
-print(listatotal);
-print("ok8");
+  // Ao ler a documentação e entender o que esta aocntecendo por de baixo dos panos 
+  //conseguimos entender o que o SET pode nos ajudar no momento da programação.
 
-   
  
   }
