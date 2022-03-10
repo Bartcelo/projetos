278188131ddd1ff33c8b6b8dd2f37866c97ecdf5
Bartcelo <44444081+Bartcelo@users.noreply.github.com>
2022-03-10 06:47:16 -0300

    10/03/22 Funções "main, objeto de primeira classe e anonimas

---
+++ aprendaDart/metodo_main/bin/metodo_main.dart
@@ -0,0 +1,24 @@
+// ?? FUnções com objeto de primeira classe
+// void printElement(int element) =>print(element);
+// void calcular (int element) => print(element *2);
+
+
+ void main() {
+// ?? função main
+// Na função main podemos passar parametreor dentro dela para iniciarmos o nosso projeto ja startando algo.
+// print(args);
+
+// ?? FUnções com objeto de primeira classe
+// ELas podem ser passadas como objetos dentro de outra função.
+// var list = [1,2,3];
+// list.forEach(printElement);
+// list.forEach(calcular);
+
+// ?? funções anonimas
+// em algumas situações podemos passar um afunção anonima como exempro temos.
+//  onchage(){}
+
+
+
+
+}
