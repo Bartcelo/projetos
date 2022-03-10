e684c490b7bffe671b70de5685fb0c8947854419
Bartcelo <44444081+Bartcelo@users.noreply.github.com>
2022-03-10 07:33:08 -0300

    10/03/22 Criando fatoraial com dart.

---
+++ aprendaDart/logica_programacao_fatorial/bin/logica_programacao_fatorial.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+ int a,b,c;
+
+print("Digite um numero para encontrar o fatotial");
+
+ a = int.parse(stdin.readLineSync()!);
+
+ for(b = 1 ; b < a; b++){
+   
+   
+ }
+print(b*a);
+
+}
